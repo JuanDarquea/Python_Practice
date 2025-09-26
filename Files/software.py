761ca52a63c3f911804e10e9a14df9002fce3005
Juan Darquea <juan.darquea.a@gmail.com>
2025-09-26 00:00:20 -0500

    Files Added From Coursera: software.py and welcome.py, they are about creating and writing files inside the Coursera practice IDE

---
+++ Files/software.py
@@ -0,0 +1,6 @@
+import numpy as np
+a = np.array([15, 5, 1])
+b = np.array([1, 3, 9])
+c = a+b
+d = c * 2
+print(d)
---
+++ Files/welcome.py
@@ -0,0 +1,10 @@
+import numpy as np 
+x = int(input())
+def array_sum(x):
+    a = np.array([x, int(2)])
+    b = np.array([int(3), x])
+    c = a + b
+    print(c)
+array_sum(x)
+message = "Hello to the world of programming"
+print(message)
