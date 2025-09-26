12a90d6df1fecf5b35b4486f955d183455a3573f
Juan Darquea <juan.darquea.a@gmail.com>
2025-09-26 12:34:00 -0500

    Subject Added(+ Files): Creating packages and reading/using them through the command line or terminal

---
+++ Files/mymath/geometry.py
@@ -0,0 +1,14 @@
+import math
+from math import pi
+def area_of_rectangle(length, width):
+    """
+    This function calculates the area of a rectangle(length * width)
+    """
+    return length * width
+
+def area_of_circle(radius):
+    """
+    This function calculates the area of a circle(pi * radius)
+    """
+    pi_value = pi
+    return pi_value * radius
