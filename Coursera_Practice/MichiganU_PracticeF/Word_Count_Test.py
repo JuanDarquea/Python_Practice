c16d89ac732b890e7770fd3ea088a1de4615bc90
Juan Darquea <juan.darquea.a@gmail.com>
2025-11-21 13:22:06 -0500

    Tried to add file reader to the word count file - in progress

--- Coursera_Practice/MichiganU_PracticeF/Word_Count_Test.py
+++ Coursera_Practice/MichiganU_PracticeF/Word_Count_Test.py
@@ -1,20 +1,29 @@
 # Word_Count_Test
+import os
+from dotenv import load_dotenv # to load environment variables from .env file
+load_dotenv()
+
+filePath = os.getenv("docx_translator_dir"), "\\TestDocument.docx"
 
 # Get the name of the file and open it
-name = input('Enter the file: ')
-handle = opent(name, 'r')
+#name = input('Enter the words: ')
+name = filePath
+handle = open(name, 'r', encoding='utf-8')
+all_words = dict()
+all_words = handle.read().text()
+handle.close()
 
 # Count word frecuency
-counts = dict()
-for line in handle:
-    words = line,split()
+count = dict()
+for line in all_words:
+    words = line.split(sep=None, maxsplit=-1)
     for word in words:
-        counts(word) = counts.get(word, 0) + 1
+        count[word] = count.get(word, 0) + 1
 
 # Find the most common word
 bigcount = None
-bigwoword = None
-for word in counts.items():
+bigword = None
+for word, count in count.items():
     if bigcount is None or count > bigcount:
         bigword = word
         bigcount = count
