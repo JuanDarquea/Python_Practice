f87c1d42e715920c07f1723fd1036839415a9758
Juan Darquea <juan.darquea.a@gmail.com>
2026-01-01 22:29:32 -0500

    Added files to check needed dependencies and a practice code about ASCII and unicode characters.

---
+++ Check_Dependencies.py
@@ -0,0 +1,6 @@
+import os
+import urllib
+from bs4 import BeautifulSoup
+from dotenv import load_dotenv
+
+print("Dependencis installed")
---
+++ Coursera_Practice/MichiganU_PracticeF/ASCII_Char.py
@@ -0,0 +1,6 @@
+print(f'Numeric value of "o" ASCII character: {ord("o")}')
+print(f'Numeric value of "l" ASCII character: {ord("l")}')
+print(f'Numeric value of "i" ASCII character: {ord("i")}')
+c = b'abc'
+x = 'abc'
+print(f'Type of b"abc": {type(c)}', f'\nType of "abc": {type(x)}')
